61cb7f9cf4b0fce481047614356fc6ae8e8ddb72
Darrell Root <darrellrootjunk@mac.com>
2019-03-01 09:07:36 -0800

    Initial Commit

---
+++ Netrek/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Netrek
+//
+//  Created by Darrell Root on 3/1/19.
+//  Copyright © 2019 Network Mom LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Netrek/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Netrek
+//
+//  Created by Darrell Root on 3/1/19.
+//  Copyright © 2019 Network Mom LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
