c514b271a8a1701e9d471b4edfb78bbd387070f8
Darrell Root <darrellrootjunk@mac.com>
2019-03-01 20:40:25 -0800

    Metaserver works

--- Netrek/AppDelegate.swift
+++ Netrek/AppDelegate.swift
@@ -11,16 +11,42 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var metaServer: MetaServer?
 
-
+    @IBOutlet weak var serverMenu: NSMenu!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        metaServer = MetaServer(hostname: "metaserver.netrek.org", port: 3521)
+        if let metaServer = metaServer {
+            metaServer.update()
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    @IBAction func refreshMetaserverMenu(_ sender: NSMenuItem) {
+        metaServer?.update()
+    }
+    public func metaserverUpdated() {
+        debugPrint("AppDelegate.metaserverUpdated")
+        if let metaServer = metaServer {
+            serverMenu.removeAllItems()
+            for (index,server) in metaServer.servers.enumerated() {
+                let newItem = NSMenuItem(title: server.description, action: #selector(self.startGame), keyEquivalent: "")
+                newItem.tag = index
+                
+                serverMenu.addItem(newItem)
+            }
+        }
+    }
+    @objc func startGame(sender: NSMenuItem) {
+        let tag = sender.tag
+        if let server = metaServer?.servers[safe: tag] {
+            print("starting game server \(server.description)")
+        }
+    }
 }
 
